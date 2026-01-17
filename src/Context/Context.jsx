fa039a3d994c2e8a5eb3fb4e22311814a8cb618c
sultani- <sultanishagufta058@gmail.com>
2026-01-17 11:24:56 -0800

    change

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -5,16 +5,37 @@
   export const Context = createContext();
 
   const ContextProvider = (props) =>{
-
+    const [ input, setInput ] = useState("");
+   const  [ recentPrompt, setRecentPrompt ] = useState("");
+   const [ prevPrompts, setPrevPrompts ] = useState([]);
+   const [ showResult, setShowResult ] = useState(false);
+   const [ loading, setLoading ] = useState(false);
+   const [ resultData, setResultData ] = useState("");
 
     const onSent = async (prompt) => {
-       await runChat(prompt)
-    }
-    onSent("What is react js");
+     setResultData("")
+      setLoading(true)
+      setShowResult(true)
+      
+      const response =  await runChat(input)
+      setResultData(response)
+      setLoading(false)
+      setInput("")
+    };
+   
 
     const contextValue = {
-
-    }
+      prevPrompts,
+      setPrevPrompts,
+      onSent,
+      recentPrompt,
+      setRecentPrompt,
+      showResult,
+      loading,
+      resultData,
+      input,
+      setInput
+    };
     return(
         <Context.Provider value={contextValue}>
             {props.children}
@@ -22,4 +43,5 @@
     );
   };
     export default ContextProvider;
+
 
