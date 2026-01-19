b657f7512c2312d4f5d726ecaac571920b249945
sultani- <sultanishagufta058@gmail.com>
2026-01-19 10:06:02 -0800

    change

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -12,14 +12,39 @@
    const [ loading, setLoading ] = useState(false);
    const [ resultData, setResultData ] = useState("");
 
+     const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData(prev=>prev+nextWord);
+    },75*index)
+
+   }
+
     const onSent = async (prompt) => {
      setResultData("")
       setLoading(true)
       setShowResult(true)
      setRecentPrompt(input)
-      
+      setPrevPrompts(prev=>[...prev,input])
       const response =  await runChat(input)
-      setResultData(response)
+
+     let responseArray = response.split("**");
+      let newResponse= "";
+      for(let i = 0; i< responseArray.length; i++)
+      {
+        if(i === 0 || i%2 !== 1){
+         newResponse += responseArray[i];
+        } 
+        else{
+          newResponse += "<br>"+responseArray[i]+"</br>";
+        }
+      }
+      let newResponse2 = newResponse.split("*").join("</br>")
+      let newResponseArray = newResponse2.split(" ");
+      for(let i=0; i<newResponseArray.length; i++)
+      {
+        const nextWord = newResponseArray[i];
+        delayPara(i, nextWord+" ")
+      }
       setLoading(false)
       setInput("")
     };
@@ -44,6 +69,7 @@
     );
   };
     export default ContextProvider;
+
 
 
 
